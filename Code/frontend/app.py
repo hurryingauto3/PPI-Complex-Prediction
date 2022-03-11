9b34cb276c844826efbccde67177e44cb52b05c9
hurryingauto3 <ah05084@st.habib.edu.pk>
2022-03-11 10:52:47 +0500

    updated

--- Code/frontend/app.py
+++ Code/frontend/app.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_navigation import Navigation
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
+
+nav = Nav()
+
+@nav.navigation()
+def mynavbar():
+    return Navbar(
+        'mysite',
+        View('Home', 'index'),
+        View('About', 'about'),
+        View('Contact', 'contact'),
+    )
+
+# ...
 
 app = Flask(__name__)
-nav = Navigation(app)
+nav.init_app(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/protnetwork')
+@app.route('/networks')
 def protnetwork():
     return render_template('protnetwork.html')
 
